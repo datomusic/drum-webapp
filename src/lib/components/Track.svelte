2b0266a7348f41598377aa45fe8276b19800ac08
David Menting <david@davidmenting.nl>
2025-05-27 18:18:53 +0200

    style: Remove background and shadow from Track component

--- src/lib/components/Track.svelte
+++ src/lib/components/Track.svelte
@@ -7,7 +7,7 @@
   export let samples: Array<{ color: string; midiNoteNumber: number }>;
 </script>
 
-<div class="flex flex-col items-center p-4 bg-gray-800  shadow-lg">
+<div class="flex flex-col items-center p-4">
   <div class="grid grid-cols-4 gap-4 sm:grid-cols-4 md:grid-cols-8">
     {#each samples as sample, i (i)}
       <SampleButton color={sample.color} midiNoteNumber={sample.midiNoteNumber} />
